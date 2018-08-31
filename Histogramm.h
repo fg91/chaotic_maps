b5e033cff0c173c70c65f1eef00922492aa3b6ff
fg91 <fabiograetz@googlemail.com>
2018-08-31 17:09:03 +0200

    pushing to github to continue work from other computer

--- Histogramm.h
+++
@@ -1,6 +0,0 @@
-// "Copyright 2018 <Fabio M. Graetz>"
-
-#ifndef HISTOGRAM_H_
-#define HISTOGRAM_H_
-
-#endif  // HISTOGRAM_H
--- main.cpp
+++ main.cpp
@@ -12,32 +12,32 @@ int main() {
   int width = 1920;
   int height = 1080;
 
-  double xmin = -1.;
-  double xmax = 1.0;
-  double ymin = -1.0;
-  double ymax = 1.5;
+  double xmin = 2.5;
+  double xmax = 4.0;
+  double ymin = 0.0;
+  double ymax = 1.0;
 
   int transient = 1000;
   int samples = 150000;
 
-  int brightness = 50;
+  int brightness = 150;
   
   
-  for (double alpha = 4.5; alpha < 5; alpha+= 0.01) {
-    std::cout << alpha << std::endl;
+  //  for (double alpha = 4.5; alpha < 5; alpha+= 0.01) {
+  //    std::cout << alpha << std::endl;
     CoordSystem coords(xmin, xmax, ymin, ymax, width, height);
-    //  LogisticMap map(0.0, 0.5, transient, samples);
-    GaussMap map(0.0, alpha, 0.5, transient, samples);
+    LogisticMap map(0.0, 0.5, transient, samples);
+    //GaussMap map(0.0, alpha, 0.5, transient, samples);
     BifurcationDiagram bD(width, height, transient, samples, brightness, coords, &map);
   
-    std::stringstream ss;
+    // std::stringstream ss;
   
-    ss << "Bild";
-    ss << alpha;
-    ss << ".bmp";
+    // ss << "Bild";
+    // ss << alpha;
+    // ss << ".bmp";
   
-    bD.drawDiagram(ss.str(), false);
-  }
+    bD.drawDiagram("test.bmp", true);
+    // }
 
 
   
